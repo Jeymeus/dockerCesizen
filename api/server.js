069e2344ff7823f1448730dad2d659d67fbde13a
Laptop\James <james.fierens@outlook.fr>
2025-04-15 11:59:34 +0200

    Initial commit - frontend + backend setup

---
+++ api/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import authRoutes from './routes/authRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Serveur API lancé sur http://localhost:${PORT}`))
